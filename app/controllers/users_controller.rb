e7fd61ee944e6d1adf577da4c9dc0badc4c201c7
mihomatsui <mihomatsui.24@gmail.com>
2021-04-02 08:24:09 +0900

    検索機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   # 1ページの表示数
   PER_PAGE = 20
+  
   def index
-    @users = User.page(params[:page]).per(PER_PAGE)
+    @q = User.ransack(params[:q])
+    @users = @q.result.page(params[:page]).per(PER_PAGE)
   end
 end
