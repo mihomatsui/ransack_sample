792226b5c8dc1077a610696c54c3783ba8c8010d
mihomatsui <mihomatsui.24@gmail.com>
2021-04-02 08:09:52 +0900

    ページネーションの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  # 1ページの表示数
+  PER_PAGE = 20
   def index
-    @users = User.limit(20)
+    @users = User.page(params[:page]).per(PER_PAGE)
   end
 end
